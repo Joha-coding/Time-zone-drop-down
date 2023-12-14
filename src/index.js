a57cb4b6d86d28f6e92be9adfe88f93c651f6f5e
Joha <jotz@outlook.fr>
2023-12-14 15:03:56 +0100

    changed alerts by on screen update
    - added paragraf with a class and id of current message
    - updated the CSS of this section
    - changed JS script : changed alerts by an on screen update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 function selectACity(event) {
   if (event.target.value.length > 0) {
     if (event.target.value === "newyork") {
-      alert(NYtime);
+      displayCurrentSelection.innerHTML = NYtime;
     }
     if (event.target.value === "paris") {
-      alert(parisTime);
+      displayCurrentSelection.innerHTML = parisTime;
     }
     if (event.target.value === "toronto") {
-      alert(torontoTime);
+      displayCurrentSelection.innerHTML = torontoTime;
     }
   }
 }
 
 let NYtimeFormat = moment()
   .tz("America/New_York")
-  .format("dddd, MMMM D, YYYY H:mA");
-let NYtime = `It is ${NYtimeFormat} in New York, USA`;
+  .format("dddd, MMMM D, YYYY H:mmA");
+let NYtime = `It is <br/> ${NYtimeFormat} <br/> in New York, USA`;
 
 let parisTimeFormat = moment()
   .tz("Europe/Paris")
-  .format("dddd, MMMM D, YYYY H:mA");
-let parisTime = `It is ${parisTimeFormat} in Paris, France`;
+  .format("dddd, MMMM D, YYYY H:mmA");
+let parisTime = `It is <br/>
+${parisTimeFormat} <br/>
+in Paris, France`;
 
 let torontoTimeFormat = moment()
   .tz("America/Toronto")
-  .format("dddd, MMMM D, YYYY H:mA");
-let torontoTime = `It is ${torontoTimeFormat} in Toronto, France`;
+  .format("dddd, MMMM D, YYYY H:mmA");
+let torontoTime = `It is <br/> ${torontoTimeFormat} <br/>in Toronto, France`;
 
 let citySelect = document.querySelector("#cities");
 citySelect.addEventListener("change", selectACity);
+
+let displayCurrentSelection = document.querySelector("#current-message");
