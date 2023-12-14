03e4b01a428a8e9011e181116680495f17f69aae
Joha <jotz@outlook.fr>
2023-12-14 14:52:50 +0100

    added JS alert for the 3 cities
    - used moment and moment timezone  libraries
    - updated alerts for Paris, New York and Toronto

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 function selectACity(event) {
   if (event.target.value.length > 0) {
     if (event.target.value === "newyork") {
-      alert("New York");
+      alert(NYtime);
     }
     if (event.target.value === "paris") {
-      alert("Paris");
+      alert(parisTime);
     }
     if (event.target.value === "toronto") {
-      alert("Toronto");
+      alert(torontoTime);
     }
   }
 }
+
+let NYtimeFormat = moment()
+  .tz("America/New_York")
+  .format("dddd, MMMM D, YYYY H:mA");
+let NYtime = `It is ${NYtimeFormat} in New York, USA`;
+
+let parisTimeFormat = moment()
+  .tz("Europe/Paris")
+  .format("dddd, MMMM D, YYYY H:mA");
+let parisTime = `It is ${parisTimeFormat} in Paris, France`;
+
+let torontoTimeFormat = moment()
+  .tz("America/Toronto")
+  .format("dddd, MMMM D, YYYY H:mA");
+let torontoTime = `It is ${torontoTimeFormat} in Toronto, France`;
 
 let citySelect = document.querySelector("#cities");
 citySelect.addEventListener("change", selectACity);
